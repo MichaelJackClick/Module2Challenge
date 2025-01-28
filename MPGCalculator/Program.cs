808643a72afd13c78e1aded4708da1747f6afe70
Jack Click <jack.click@outlook.com>
2025-01-28 08:19:50 +0000

    Initial Commit

---
+++ MPGCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿int milesDriven; 
+int gallonsUsed;
+double mpg;
+
+Console.Write("Enter the number of miles driven: ");
+milesDriven = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the gallons of gas used: ");
+gallonsUsed = int.Parse(Console.ReadLine());
+
+mpg = (double)milesDriven / gallonsUsed;
+
+Console.WriteLine($"The miles per gallon (MPG) is {mpg}");
